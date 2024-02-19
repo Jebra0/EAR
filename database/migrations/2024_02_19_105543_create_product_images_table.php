2cb8ca424a53eb6fbfeef640dfca4b07d767d0f6
jabrallah <mohammed01110834150@gmail.com>
2024-02-19 15:09:58 +0200

    create the important tables

--- database/migrations/2024_02_19_105543_create_product_images_table.php
+++ database/migrations/2024_02_19_105543_create_product_images_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('product_images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->boolean('is_admin')->default(false);
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->string('image');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('product_images');
     }
 };
